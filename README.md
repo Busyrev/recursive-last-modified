e5d2a21702d6cb9dfff4f0645d48fd2c24f733af
Mike Busyrev <busyrev@gmail.com>
2018-04-12 15:46:54 +0300

    first implementation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # recursive-last-modified
 Walks through specified directory or directories and returns last change date of all tree. Tecnically it is the newest modified or created date of all files in specified dir
+# future plans
+* node-glob syntax support  https://github.com/isaacs/node-glob
+* excludes
---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * Walks through specified directory or directories and returns last change date of all tree.
+ * Tecnically it is the newest modified or created date of all files in specified dir.
+ * @param {string|Array.<string>} title - The title of the book.
+ */
+export function recursiveLastModified(dir) {
+    if (Array.isArray(dir)) {
+        return Math.max.apply(null, dir.map(d => {
+            checkArg(d);
+            return getLastModifiedRecursive(d);
+        }
+        ));
+    } else {
+        return getLastModifiedRecursive(p);
+    }
+
+    function checkArg(dir) {
+        if (typeof dir == 'string') {
+            throw new Error('Only string or array of string supported in parameter');
+        }
+    }
+    let path = require('path');
+    let fs = require('fs');
+
+
+    function getLastModifiedRecursive(candidate) {
+        let stats = fs.statSync(candidate);
+        let candidateTimes = [stats.mtimeMs, stats.ctimeMs, stats.birthtimeMs];
+        if (stats.isDirectory()) {
+            let files = fs.readdirSync(candidate);
+            candidateTimes = candidateTimes.concat(files.map(f => getLastModifiedRecursive(path.join(candidate, f))));
+        }
+        return Math.max.apply(null, candidateTimes);
+    }
+}
