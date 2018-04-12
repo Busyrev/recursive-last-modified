e8e3d1880a7c2cacd6ecc5bc271baa53a4d31469
Mike Busyrev <busyrev@gmail.com>
2018-04-12 16:32:16 +0300

    fixes and updateted docs. Now works!

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 /**
  * Walks through specified directory or directories and returns last change date of all tree.
  * Tecnically it is the newest modified or created date of all files and dirs in specified dir.
- * @param {string|Array.<string>} title - The title of the book.
+ * @param {string|Array.<string>} title - stringPath or array of stringPaths to find last modified date recursive
+ * @return {number} - fractional number of unix timestamp in MILLISECONDS
  */
 module.exports = function recursiveLastModified(dir) {
+    let path = require('path');
+    let fs = require('fs');
+    
     if (Array.isArray(dir)) {
         return Math.max.apply(null, dir.map(d => {
+            console.log(d);
             checkArg(d);
             return getLastModifiedRecursive(d);
         }
@@ -14,15 +19,12 @@ module.exports = function recursiveLastModified(dir) {
         return getLastModifiedRecursive(dir);
     }
 
-    function checkArg(dir) {
-        if (typeof dir == 'string') {
+    function checkArg(candidate) {
+        if (typeof candidate !== 'string') {
             throw new Error('Only string or array of string supported in parameter');
         }
     }
-    let path = require('path');
-    let fs = require('fs');
-
-
+    
     function getLastModifiedRecursive(candidate) {
         let stats = fs.statSync(candidate);
         let candidateTimes = [stats.mtimeMs, stats.ctimeMs, stats.birthtimeMs];
